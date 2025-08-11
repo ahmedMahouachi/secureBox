d44aa5b221b6b25084c242149b967dce252c22e2
ahmedMahouachi <ahmedmahouachi66@gmail.com>
2025-08-12 00:28:14 +0100

    sara: index.html

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path'); // needed to join paths
+
 const { connectDB } = require('./database/db');
 
 const app = express();
 app.use(express.json());
+
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 const port = process.env.PORT || 3000;
